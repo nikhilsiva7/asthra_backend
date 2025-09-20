cff5a56f827680e38d240f9f3312ecf78692a0d1
nikhilsiva7 <nikhilsivavoleti@gmail.com>
2025-09-20 19:18:38 +0530

    small change

--- account/views.py
+++ account/views.py
@@ -1,16 +1,21 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
 from .serializers import RegisterSerializer, LoginSerializer
 from .models import User
+
 
 class RegisterView(APIView):
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            return Response({"message": "User registered successfully"})
-        return Response(serializer.errors, status=400)
+            return Response(
+                {"message": "User registered successfully"},
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LoginView(APIView):
@@ -19,10 +24,13 @@ class LoginView(APIView):
         if serializer.is_valid():
             user = serializer.validated_data
             refresh = RefreshToken.for_user(user)
-            return Response({
-                "username": user.username,
-                "role": user.role,
-                "refresh": str(refresh),
-                "access": str(refresh.access_token),
-            })
-        return Response(serializer.errors, status=400)
+            return Response(
+                {
+                    "username": user.username,
+                    "role": user.role,
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                },
+                status=status.HTTP_200_OK,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
